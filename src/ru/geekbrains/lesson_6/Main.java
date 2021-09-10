9d07de8a95cdecc5a3806f3f946ae156c240d576
eleonorchuk <zoimy@ukr.net>
2021-09-10 22:52:36 +0300

    lesson 2 task 3

--- src/ru/geekbrains/lesson_6/Main.java
+++ src/ru/geekbrains/lesson_6/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // test task 1
         Random rnd = new Random();
         boolean result = testSumBetween1020(0,20);
         System.out.println("testSumBetween1020 returned " + result );
@@ -12,9 +13,19 @@ public class Main {
         System.out.println("testSumBetween1020 returned " + result );
         result = testSumBetween1020(rnd.nextInt(30), rnd.nextInt(30));
         System.out.println("testSumBetween1020 returned " + result );
+
+        // test task 2
         printNumberSign(20);
         printNumberSign(-120);
         printNumberSign(rnd.nextInt(100) - 50);
+
+        // test task 3
+        result = testInvertNumberSign(987);
+        System.out.println("testInvertNumberSign returned " + result );
+        result = testInvertNumberSign(-560);
+        System.out.println("testInvertNumberSign returned " + result );
+        result = testInvertNumberSign(rnd.nextInt(30) - 15);
+        System.out.println("testInvertNumberSign returned " + result );
     }
 
     /*
@@ -43,10 +54,22 @@ public class Main {
     public static void printNumberSign(int a) {
         System.out.println("printNumberSign");
         System.out.println("a = " + a);
-        
+
         if (a >= 0)
             System.out.println("Положительное число");
         else
             System.out.println("Отрицательное число");
     }
+
+    /*
+    3. Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число
+    отрицательное, и вернуть false если положительное.
+      */
+    public static boolean testInvertNumberSign(int a) {
+        System.out.println("testInvertNumberSign");
+        System.out.println("a = " + a);
+
+        return a < 0;
+    }
 }
+
